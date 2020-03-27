c9b806e29880fd527004f6ce1c5489d3debdbbd6
Jozy Kinnaman <jozypants@gmail.com>
2020-03-27 11:23:47 -0700

    add var function declarations to sctipts.js for form input types.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ $(document).ready(function() {
   $("form#language").submit(function(event) {
     var age = parseInt($("input#age").val());
     console.log(age);
+    var preference = $("#preference").val();
+    console.log(preference);
+    var brainType = $("input:radio[name=brainType]:checked").val();
+    console.log(brainType);
+    var startDate = $("#startDate").val();
+    console.log(startDate);
+    var favoriteColor = $("#color").val();
+    console.log(favoriteColor);
     event.preventDefault();
   });
 });
